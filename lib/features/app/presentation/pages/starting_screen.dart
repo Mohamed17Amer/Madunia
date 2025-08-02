5c0f1502a2ef038d0699d08d7d0dfaa327901e87
Mohamed Abd EL-Moneim Amer <138235067+Mohamed17Amer@users.noreply.github.com>
2025-08-02 03:42:16 +0200

    repair request screen ui done

--- lib/features/app/presentation/pages/starting_screen.dart
+++ lib/features/app/presentation/pages/starting_screen.dart
@@ -4,7 +4,6 @@ import 'package:madunia/core/utils/colors/app_colors.dart';
 import 'package:madunia/features/app/presentation/view/widgets/custom_bottom_nav_bar.dart';
 import 'package:madunia/features/app/presentation/view_model/cubit/app_cubit.dart';
 
-
 class StartingScreen extends StatefulWidget {
   const StartingScreen({super.key});
 
@@ -20,7 +19,7 @@ class _StartingScreenState extends State<StartingScreen> {
       textDirection: TextDirection.rtl,
       child: Scaffold(
         backgroundColor: Colors.transparent,
-      
+
         body: Stack(
           // for background and body
           children: [
@@ -34,14 +33,14 @@ class _StartingScreenState extends State<StartingScreen> {
                   colors: AppColors.homeGradientColorsList,
                 ),
               ),
-      
+
               child: BlocBuilder<AppCubit, AppState>(
                 builder: (context, state) {
                   return AppCubit.pagesViews[AppCubit.currentIndex];
                 },
               ),
             ),
-      
+
             // for nav bar
             CustomBottomNavBar(),
           ],
